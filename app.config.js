57c851d7a99e1fc019923a2e9a32a7f0565d5a8f
aenrione <aenrione@protonmail.com>
2022-05-11 23:12:40 -0400

    add proper support for .env

---
+++ app.config.js
@@ -0,0 +1,14 @@
+import 'dotenv/config';
+
+export default {
+  name: 'budget-tracker',
+  version: '1.0.0',
+  icon: "./assets/images/wallet.png",
+  assetBundlePatterns: [
+    "**/*"
+  ],
+  android: {
+    package: "aenrione.budget"
+  }
+
+};
--- babel.config.js
+++ babel.config.js
@@ -3,13 +3,14 @@ module.exports = function(api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
+      ['inline-dotenv'],
       [
         "module:react-native-dotenv",
         {
-            moduleName: "@env",
-            path: ".env",
+          moduleName: "@env",
+          path: ".env",
         },
-    ],
+      ],
       [
         "module-resolver",
         {
